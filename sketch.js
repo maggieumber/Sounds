8d86e553c7ac2792ae14b6dcb82908c313eb9911
maggieumber <maggieumber@gmail.com>
2020-02-03 09:39:21 -0600

    made multiple sounds play together with key pressed

--- sketch.js
+++ sketch.js
@@ -1,30 +1,52 @@
 let titleImage;
-let song1, song2, song3;
-let songs;
-let index = 0;
+let sound1, sound4, sound5;
 
 function preload() {
   titleImage = loadImage('/assets/intoxicated-0001.jpg');
-  song1 = loadSound('/assets/476202__djmistressm__rain-drips.mp3');
-  song2 = loadSound('482826__craigsmith__r15-45-heart-beat.wav ');
-  song3 = loadSound('482830__craigsmith__r16-07-woman-laughing-and-group-of-men-laughing.wav');
+  sound1 = loadSound('/assets/476202__djmistressm__rain-drips.mp3');
+  sound4 = loadSound('assets/271447__robinhood76__05725-drunken-party-ambience.wav');
+  sound5 = loadSound('assets/475647__o-ciz__hallway-1-people-chatting-in-another-room-voices.wav ');
 }
 
 function setup() {
   createCanvas(400, 400);
-  songs = [song1, song2, song3];
 }
 
 function draw() {
   background(255);
   imageMode(CENTER);
   image(titleImage, width / 2, height / 2);
-  songs.play();
 }
 
-function mousePressed() {
-  index = index + 1;
-  if (index == songs.length) {
-    index = 0;
+
+function keyTyped() {
+  if (key == "a") {
+    sound1.play();
+  } else if (key == "q") {
+    sound1.pause();
+  } else if (key == "l") {
+    sound4.play();
+  } else if (key == "o") {
+    sound4.pause();
+  } else if (key == "g") {
+    sound5.play();
+  } else if (key == "b") {
+    sound5.pause();
   }
 }
+
+// // drunken voices play & pause
+// function mousePressed() {
+//   sound4.pause();
+// }
+
+// // muffled voices play & pause
+// function mousePressed() {
+//   sound5.play();
+// }
+
+// function keyTyped() {
+//   if (key == 'p') {
+//     sound5.pause();
+//   }
+// }
