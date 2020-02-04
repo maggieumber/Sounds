5e07f8360b1ad7e66c096e2841edf9ea45d35e4b
maggieumber <maggieumber@gmail.com>
2020-02-03 19:22:26 -0600

    testing new sound code

--- sketch.js
+++ sketch.js
@@ -1,20 +1,10 @@
-let titleImage;
-// let rain;
-// , drukenParty, muffledParty, breathing, creature;
+let title;
+let rain, underwater;
 
 function preload() {
-  titleImage = loadImage('/assets/intoxicated-0001.jpg');
-  // SOUND CREDITS
-  // Rain Drips by DJMistressM https: //freesound.org/people/DJMistressM/sounds/476202/ This work is licensed under the Creative Commons 0 License.
-  // rain = loadSound('/assets/476202__djmistressm__rain-drips.mp3');
-  // 05725 drunken party ambience by Robinhood76 (https://freesound.org/people/Robinhood76/sounds/271447/) MU edited this into a shorter version. This work is licensed under the Attribution Noncommercial License. https://creativecommons.org/licenses/by-nc/3.0/
-  // drukenParty = loadSound('assets/476727__lipatovroman__bar-crowd-loop-01.wav');
-  // // // Hallway_1(people chatting in another room, voices).wav by o_ciz https://freesound.org/people/o_ciz/sounds/475647/ This work is licensed under the Creative Commons 0 License.
-  // muffledParty = loadSound('assets/183893__ryanconway__underwater-breathing.mp3');
-  // // Muffled breath by the hands.wav by fuchsrodolfo https://freesound.org/people/fuchsrodolfo/sounds/391174/ This work is licensed under the Attribution Noncommercial License. https://creativecommons.org/licenses/by-nc/3.0/
-  // // breathing = loadSound('assets/391174__fuchsrodolfo__muffled-breath-by-the-hands.wav')
-  // // // chainedcreature2.mp3 by johnaudiotech https://freesound.org/people/johnaudiotech/sounds/342117/ This work is licensed under the Creative Commons 0 License.
-  // creature = loadSound('assets/342117__johnaudiotech__chainedcreature2.mp3')
+  title = loadImage("assets/intoxicated-0001.jpg");
+  rain = loadSound("assets/476202__djmistressm__rain-drips.mp3");
+  underwater = loadSound("assets/183893__ryanconway__underwater-breathing.mp3");
 }
 
 function setup() {
@@ -24,32 +14,13 @@ function setup() {
 function draw() {
   background(255);
   imageMode(CENTER);
-  image(titleImage, width / 2, height / 2);
+  image(title, 0, 0);
 }
 
-
-// function keyTyped() {
-//   if (key == "a") {
-//     rain.play();
-//   } else if (key == "q") {
-//     rain.pause();
-//   }
-//   /* else if (key == "s") {
-//      drukenParty.loop();
-//    } else if (key == "w") {
-//      drukenParty.pause();
-//    } else if (key == "d") {
-//      muffledParty.loop();
-//    } else if (key == "e") {
-//      muffledParty.pause();
-//    } else if (key == "f") {
-//       breathing.loop();
-//     } else if (key == "r") {
-//       breathing.pause();
-//     }
-//    else if (key == "g") {
-//      creature.loop();
-//    } else if (key == "t") {
-//      creature.pause();
-//    } */
-// }
+function keyTyped() {
+  if (key == "a") {
+    rain.play();
+  } else if (key == "l") {
+    underwater.play();
+  }
+}
